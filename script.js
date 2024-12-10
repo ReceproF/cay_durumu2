f4540f6312209cb5c2a697e133ca82e587175b39
ReceproF <laburazkuya@hotmail.com>
2024-12-10 19:43:30 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function setTeaStatus(status) {
+    // Çay durumu ayarlanacak
+}
+
+function setTeaTimer() {
+    // Çay sayacı ayarlanacak
+}
+
+function setWaterStatus(status) {
+    // Su durumu ayarlanacak
+}
+
+function setWaterTimer() {
+    // Su sayacı ayarlanacak
+}
+
+// Sayaçları ve durumları güncelleme işlevleri burada olacak
